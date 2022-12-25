5dbf307059af7ad92bc585392c2e309557428dce
Dream <sdeepicivil@gmail.com>
2022-12-25 23:22:21 +0530

    Signup Front end Task 1

---
+++ public/js/signup.js
@@ -0,0 +1,19 @@
+
+document.getElementById('submit').addEventListener('submit',postSignUp);
+
+function postSignUp(event){
+    const name= event.target.name;
+    const mail = event.target.mail;
+    const phone = event.target.phone;
+    const password = event.target.password;
+
+    const obj = {name,mail,phone,password};
+
+    axios.post('http://localhost:3000/signup',obj)
+    .then((res)=>{
+        document.getElementById('error').value="User has been created successfully";
+    })
+    .catch((err)=>{
+        document.getElementById('error').value="User Exists already"; 
+    });
+}
