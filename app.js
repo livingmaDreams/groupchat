bf2a1bd420c06a35e033b4d9cfebdcc043acf1e3
Dream <sdeepicivil@gmail.com>
2022-12-30 15:51:16 +0530

    Signup

--- app.js
+++ app.js
@@ -4,21 +4,21 @@ const app = express();
 const bp = require('body-parser');
 app.use(bp.json());
 
-const dotenv = require('dotenv');
-dotenv.config();
+// const dotenv = require('dotenv');
+// dotenv.config();
 
-const cors = require('cors');
-app.use(cors());
+// const cors = require('cors');
+// app.use(cors());
 
 const path = require('path');
 app.use(express.static(path.join(__dirname,'public')));
 
-const morgan = require('morgan');
-const fs = require('fs');
-const accessLogStream = fs.createWriteStream(
-    path.join(__dirname,'access.log'),
-    {flags: 'a'});
-app.use(morgan('combined',{stream: accessLogStream}));
+// const morgan = require('morgan');
+// const fs = require('fs');
+// const accessLogStream = fs.createWriteStream(
+//     path.join(__dirname,'access.log'),
+//     {flags: 'a'});
+// app.use(morgan('combined',{stream: accessLogStream}));
 
 const signUpRouter = require('./routes/signup');
 app.use('/signup',signUpRouter);
--- controllers/signup.js
+++ controllers/signup.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 exports.getSignUpPage = (req,res,next) => {
     res.sendFile(path.join(__dirname,'../views/signup.html'));
 }
